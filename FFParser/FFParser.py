569f8a6c7a24a3a45c26c4cf9530c9c657a98bd5
Negaihoshi <e222et@gmail.com>
2014-05-17 23:19:07 +0800

    v0.2 add export json_data function

--- FFParser/FFParser.py
+++ FFParser/FFParser.py
@@ -8,9 +8,11 @@ import requests
 import re
 
 def BSPaser(req):
-	#print(req)
-	soup = BeautifulSoup(req)
 
+	list = []
+	list2 = []
+
+	soup = BeautifulSoup(req)
 	webtext = soup.get_text().strip().split()
 
 	print(soup.head.title)
@@ -26,11 +28,22 @@ def BSPaser(req):
 	}
 	id = 0
 
+	jsonFile = open("data.json","w+",encoding="utf-8")
+	jsonFile.write("[\n")
+
 	for x in range(5,len(table),5):
 		id+=1
-		datastruct['社團'] = re.match('^.+>(.+)<', str(table[x])).groups()
+		datastruct['社團'] = re.match('^.+>(.+)<', str(table[x])).groups()[0]
 		datastruct['id'] = id
-		print(datastruct['id'],datastruct['社團'])
+		#print(datastruct['id'],datastruct['社團'])
+		toJson = json.dumps(datastruct, ensure_ascii=False, indent=2)
+		jsonFile.write(toJson)
+		if datastruct['id'] != 744: jsonFile.write(",")
+
+	jsonFile.write("\n]")
+	jsonFile.close()
+
+
 
 def WebRequest(url):
 
@@ -44,3 +57,5 @@ def WebRequest(url):
 
 if __name__ == "__main__":
 	WebRequest('http://www.f-2.com.tw/index.php?q=ff/44836')
+
+
